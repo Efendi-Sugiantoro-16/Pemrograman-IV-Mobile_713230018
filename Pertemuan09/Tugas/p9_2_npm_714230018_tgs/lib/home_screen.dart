19812b95d83d3047cb2839ac07bb26ed8c47f0ce
Efendi-Sugiantoro-16 <efendisugiantoro16@gmail.com>
2025-12-13 00:21:07 +0700

    upload source code

---
+++ Pertemuan09/Tugas/p9_2_npm_714230018_tgs/lib/home_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart'; 
+import 'login_screen.dart'; 
+ 
+class MyHome extends StatefulWidget { 
+  const MyHome({super.key}); 
+ 
+  @override 
+  State<MyHome> createState() => _MyHomeState(); 
+} 
+ 
+class _MyHomeState extends State<MyHome> { 
+
+  late SharedPreferences loginData;
+
+  String username = "";
+
+  void initial() async {
+    loginData = await SharedPreferences.getInstance();
+    setState(() {
+      username = loginData.getString('username') ?? "";
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initial();
+  }
+  
+
+  @override 
+  Widget build(BuildContext context) { 
+    return Scaffold( 
+      appBar: AppBar( 
+        title: const Text('Home'), 
+      ), 
+      body: Center( 
+        child: Container( 
+          margin: const EdgeInsets.symmetric( 
+            vertical: 12, 
+            horizontal: 16, 
+          ), 
+          child: Column( 
+            children: [ 
+              const Text('Welcome to Home'), 
+              const SizedBox(height: 20), 
+              Text(username), 
+              ElevatedButton(
+                onPressed: () {
+                  bool remember = loginData.getBool('rememberMe') ?? false;
+
+                  loginData.setBool('login', true);
+
+                  if (!remember) {
+                    loginData.remove('username');
+                    loginData.remove('remembered_username');
+                  }
+
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginScreen()),
+                  );
+                },
+                child: const Text('Logout'),
+              )
+            ], 
+          ), 
+        ), 
+      ), 
+    ); 
+  } 
+} 
